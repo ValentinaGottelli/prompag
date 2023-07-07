c19be5149c277b062cf19f94fa6a98f45f9213ca
ValentinaGottelli <valentinagottelli@gmail.com>
2023-07-06 21:34:43 -0300

    first

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "D:\\promptopia\\promptopia\\app\\layout.jsx": [
+      "D:\\promptopia\\promptopia\\styles\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "D:\\promptopia\\promptopia\\app\\page": [
+      "D:\\promptopia\\promptopia\\styles\\globals.css"
+    ]
+  }
+}
---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth ({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+        })
+    ],
+    async session({session}){
+
+    },
+    async signIn({profile}){
+        try{
+            await connectToDB();
+
+        //chequear si el usuario existe
+        // si no existe, crear nuevo usuario
+
+        return true;
+        }catch(error){
+            console.log(error);
+            return false;
+        }
+    }
+})
+
+export {handler as GET, handler as POST};
